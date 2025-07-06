8663524c2c89a954f28fa9a0778b3545e4e1541a
xuanyuzhu960 <zhuxuanyu960@gmail.com>
2025-07-07 00:37:17 +0800

    Update Train_PhaseNet3D.py

--- PhaseNet3D/Train_PhaseNet3D.py
+++ PhaseNet3D/Train_PhaseNet3D.py
@@ -110,6 +110,42 @@ def Laploss(input, recon_unimg, device=torch.device("cuda:0" if torch.cuda.is_av
 
     return Laploss, Diff
 
+def TVLoss(image, mask):
+
+    diff_x = torch.diff(image, dim=2)
+    diff_y = torch.diff(image, dim=3)
+    diff_z = torch.diff(image, dim=4)
+
+    diff_mask_x = torch.diff(mask, dim=2)
+    diff_mask_y = torch.diff(mask, dim=3)
+    diff_mask_z = torch.diff(mask, dim=4)
+
+    # diff_x_pad = pad(diff_x ** 2, [0, 0, 0, 0, 0, 1])
+    # diff_y_pad = pad(diff_y ** 2, [0, 0, 0, 1])
+    # diff_z_pad = pad(diff_z ** 2, [0, 1])
+
+    diff_x_pad = pad(diff_x, [0, 0, 0, 0, 0, 1])
+    diff_y_pad = pad(diff_y, [0, 0, 0, 1])
+    diff_z_pad = pad(diff_z, [0, 1])
+
+    diff_maskx_pad = pad(diff_mask_x, [0, 0, 0, 0, 0, 1])
+    diff_masky_pad = pad(diff_mask_y, [0, 0, 0, 1])
+    diff_maskz_pad = pad(diff_mask_z, [0, 1])
+
+    loss_3D = torch.abs((1 - diff_maskx_pad) * diff_x_pad) + torch.abs((1 - diff_masky_pad)*diff_y_pad) + torch.abs((1 - diff_maskz_pad) * diff_z_pad)
+
+    true_loss = torch.sum(loss_3D)
+
+    shape_image = image.shape
+    dimx = shape_image[2]
+    dimy = shape_image[3]
+    dimz = shape_image[4]
+
+    loss = true_loss/(dimx*dimy*dimz)
+
+    # gradient = torch.sqrt((diff_x_pad ** 2 + diff_y_pad ** 2 + diff_z_pad ** 2)/3)
+
+    return loss
 
 def SaveNet(PhaseNet, epo, enSave = False):
     print('save results')
@@ -185,7 +221,6 @@ def TrainNet(PhaseNet, LR = 0.001, Batchsize = 24, Epoches = 45, useGPU = True):
                     # max_pred_label = torch.max(Predictions, dim=1)[0].unsqueeze(1)
                     Max_Prediction = torch.max(Predictions, dim=1)[1].unsqueeze(1)  # [24,1,64,64,64]
                     Prediction_OriBase = Max_Prediction - 5  # Base RE-Shifting Ver.2
-                    # Tissue mask?
                     # print('Dim of Max_Prediction')
                     # print(Max_Prediction.size())
 
@@ -205,7 +240,6 @@ def TrainNet(PhaseNet, LR = 0.001, Batchsize = 24, Epoches = 45, useGPU = True):
                     ## loss
                     loss1 = criterion1(Predictions, Labels)
                     loss2 = MskedResidueLoss(Imagess, Recon_UWP_Orig)  # XYZ edit ## Breakpoint2
-                    # loss2 = Laploss(Imagess, Recon_UWP_Orig)  # XYZ edit ## Breakpoint2
                     loss3 = criterion2(Recon_UWP_Shift, Label_UWPs)
 
                     # loss2 = loss2.mean()
